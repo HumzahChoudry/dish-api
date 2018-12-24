b8624ff29021474d8923e8e04e090eeae6101936
humzahchoudry <Humzah.choudry@gmail.com>
2018-12-23 21:20:23 -0600

    got api to work

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::RestaurantsController < ApplicationController
 
+  def test
+    byebug
+  end
   def query_restaurants
-    restaurants_hopefully = Restaurant.get_restaurants_from_text(params[:slug])
+    restaurants_hopefully = Restaurant.get_auto_complete_responses(params[:slug])
     puts restaurants_hopefully
     render json: restaurants_hopefully
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get 'test', to: 'restaurants#test'
   namespace :api do
     namespace :v1 do
       get '/restaurants/search/:slug', to: 'restaurants#query_restaurants'
