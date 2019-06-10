fb87988881e0839f32dcf6e4dd6fe0aeccdce39e
humzahchoudry <Humzah.choudry@gmail.com>
2019-06-10 14:59:56 -0400

    updated serializers and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_184450) do
+ActiveRecord::Schema.define(version: 2019_05_06_231822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_184450) do
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
+    t.string "picture_url"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
